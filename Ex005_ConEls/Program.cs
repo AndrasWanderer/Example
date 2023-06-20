e83c7c0bc04154704417bc0527d783f3d0c10fa6
Andras <warlock61636@yandex.ru>
2023-06-21 00:28:53 +0300

    Ex005

---
+++ Ex005_ConEls/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter the username: ");
+string username = Console.ReadLine();
+if(username == "Andras")
+{
+    Console.WriteLine("Vallar Morgulis, Andras.");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
