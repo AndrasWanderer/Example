f660e9cd45c5d2cfa8734724377abb9400a42fc3
Andras <warlock61636@yandex.ru>
2023-06-21 00:41:10 +0300

    Ex006

---
+++ Ex006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 18;
+int b = 56;
+int c = 73;
+int d = 24;
+int e = 32;
+
+int max = a;
+
+if(a>max)max=a;
+if(b>max)max=b;
+if(c>max)max=c;
+if(d>max)max=d;
+if(e>max)max=e;
+
+Console.Write("max=");
+Console.WriteLine(max);
